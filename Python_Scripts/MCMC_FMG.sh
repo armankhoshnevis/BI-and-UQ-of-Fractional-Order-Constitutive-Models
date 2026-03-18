43b546b2277cab46e8e1d8ba1593beb8c4dd0b1b
armankhoshnevis <khoshne1@msu.edu>
2026-03-18 09:59:44 -0400

    Add slurm bash script

---
+++ Python_Scripts/MCMC_FMG.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=MCMC_FMG
+
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --time=01:00:00
+
+# Load modules and activate conda environment
+ml purge
+ml load Miniforge3
+ml load powertools
+conda activate UQ_Project
+
+# Run the Python script
+python MCMC_FMG_Inference.py \
+  --HS 20
+
+# python MCMC_FMG_Inference_PostProcessing.py \
+#   --HS 20
+
+echo "Job started at $(date)"
+echo "Running on host $(hostname)"
+srun python MCMC_FMG_20HSWF.py
+echo "Job finished at $(date)"
